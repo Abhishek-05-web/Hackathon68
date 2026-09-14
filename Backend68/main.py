f17b1f3f104403088a67a7c2daefd80a73d4cb76
Abhishek-05-web <abhishek18kr2007@gmail.com>
2026-09-14 11:15:35 +0530

    backend live

--- Backend68/main.py
+++ Backend68/main.py
@@ -6,30 +6,18 @@ import weather_service
 
 
 app = FastAPI(title="WeatherGPT API")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 # Frontend ko backend access dene ke liye
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://abhishek-05-web.github.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # Frontend se ye data aayega
 class ChatRequest(BaseModel):
